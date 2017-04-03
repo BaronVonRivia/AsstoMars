49d80fba4048f990981947677e7f8d692fdce1a6
jason_gomez2003@msn.com <jason_gomez2003@msn.com>
2017-04-03 11:51:36 -0700

    Added Bullet Trail and walkable foreground

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -7,6 +7,9 @@ public class Weapon : MonoBehaviour {
 	public float Damage = 10;
 	public LayerMask whatToHit;
 
+	public Transform BulletTrailPrefab;
+	float timeToSpawnEffect = 0;
+	public float effectSpawnRate = 10;
 	private float timeToFire = 0;
 	Transform firePoint;
 
@@ -37,10 +40,19 @@ public class Weapon : MonoBehaviour {
 		Vector2 mousePosition = new Vector2 (Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
 		Vector2 firePointPosition = new Vector2 (firePoint.position.x, firePoint.position.y);
 		RaycastHit2D hit = Physics2D.Raycast (firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
+		if (Time.time >= timeToSpawnEffect) {
+			Effect ();
+			timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
+		}
 		Debug.DrawLine (firePointPosition, (mousePosition-firePointPosition)*100, Color.cyan);
 		if (hit.collider != null) {
 			Debug.DrawLine (firePointPosition, hit.point, Color.red);
 			Debug.Log ("We hit " + hit.collider.name + " and did " + Damage + " damage.");
 		}
-	}	
+	}
+
+	void Effect () {
+		Instantiate (BulletTrailPrefab, firePoint.position, firePoint.rotation);
+	}
+
 }

18ac61a9e41fa4d159eb44ce125e44ada49f26d3
jason_gomez2003@msn.com <jason_gomez2003@msn.com>
2017-04-03 17:41:26 -0700

    Added Muzzle Flash

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour {
 	public LayerMask whatToHit;
 
 	public Transform BulletTrailPrefab;
+	public Transform MuzzleFlashPrefab;
 	float timeToSpawnEffect = 0;
 	public float effectSpawnRate = 10;
 	private float timeToFire = 0;
@@ -53,6 +54,11 @@ public class Weapon : MonoBehaviour {
 
 	void Effect () {
 		Instantiate (BulletTrailPrefab, firePoint.position, firePoint.rotation);
+		Transform clone = Instantiate (MuzzleFlashPrefab, firePoint.position, firePoint.rotation) as Transform;
+		clone.parent = firePoint;
+		float size = Random.Range (0.6f, 0.9f);
+		clone.localScale = new Vector3 (size, size, size);
+		Destroy (clone.gameObject, 0.02f);
 	}
 
 }

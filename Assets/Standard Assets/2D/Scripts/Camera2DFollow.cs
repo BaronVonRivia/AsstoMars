e07082df45541f9fa2f003ccd40560d7c9016dcf
jason_gomez2003@msn.com <jason_gomez2003@msn.com>
2017-04-09 19:08:08 -0700

    Added Player Health

--- Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -10,6 +10,7 @@ namespace UnityStandardAssets._2D
         public float lookAheadFactor = 3;
         public float lookAheadReturnSpeed = 0.5f;
         public float lookAheadMoveThreshold = 0.1f;
+		public float yPosRestriction = -1;
 
         private float m_OffsetZ;
         private Vector3 m_LastTargetPosition;
@@ -28,6 +29,10 @@ namespace UnityStandardAssets._2D
         // Update is called once per frame
         private void Update()
         {
+
+			if (target == null)
+				return;
+
             // only update lookahead pos if accelerating or changed direction
             float xMoveDelta = (target.position - m_LastTargetPosition).x;
 
@@ -44,6 +49,8 @@ namespace UnityStandardAssets._2D
 
             Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
+
+			newPos = new Vector3 (newPos.x, Mathf.Clamp(newPos.y, yPosRestriction, Mathf.Infinity), newPos.z);
 
             transform.position = newPos;
 

3b66ca4ff9e2e2395075afe271e2c435d29b28ac
RoccoEstrada <RoccoEstrada>
2017-04-21 17:22:00 -0700

    Added hit particles

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,6 +5,7 @@ using Pathfinding;
 [RequireComponent(typeof (Rigidbody2D))]
 [RequireComponent(typeof (Seeker))]
 public class EnemyAI : MonoBehaviour {
+	public static GameMaster gm;
 	//What to chace?
 	public Transform target;
 
@@ -30,6 +31,8 @@ public class EnemyAI : MonoBehaviour {
 	private bool searchingForPlayer = false;
 
 	void Start(){
+		gm = FindObjectOfType<GameMaster> ();
+		//GameObject go = GameObject.FindGameObjectWithTag("Player");
 		seeker = GetComponent<Seeker> ();
 		rb = GetComponent<Rigidbody2D> ();
 
@@ -121,6 +124,14 @@ public class EnemyAI : MonoBehaviour {
 			return;
 		}
 
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "Player") {
+			Destroy (other.gameObject);
+			gm.StartCoroutine (gm.RespawnPlayer ());
+
+		}
 	}
 
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,6 +9,7 @@ public class Weapon : MonoBehaviour {
 	public AudioClip gunAudio;
 
 	public Transform BulletTrailPrefab;
+	public Transform HitPrefab;
 	public Transform MuzzleFlashPrefab;
 	float timeToSpawnEffect = 0;
 	public float effectSpawnRate = 10;
@@ -46,28 +47,51 @@ public class Weapon : MonoBehaviour {
 		Vector2 mousePosition = new Vector2 (Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
 		Vector2 firePointPosition = new Vector2 (firePoint.position.x, firePoint.position.y);
 		RaycastHit2D hit = Physics2D.Raycast (firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
-		if (Time.time >= timeToSpawnEffect) {
-			Effect ();
-			timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
-		}
+
 		Debug.DrawLine (firePointPosition, (mousePosition-firePointPosition)*100, Color.cyan);
 		if (hit.collider != null) {
 			Debug.DrawLine (firePointPosition, hit.point, Color.red);
 			Debug.Log ("We hit " + hit.collider.name + " and did " + Damage + " damage.");
-			SEnemy senemy = hit.collider.GetComponent<SEnemy>();
-			Enemy enemy = hit.collider.GetComponent<Enemy>();
+			SEnemy senemy = hit.collider.GetComponent<SEnemy> ();
+			Enemy enemy = hit.collider.GetComponent<Enemy> ();
 			if (senemy != null) {
-				senemy.DamageSEnemy(Damage);
+				senemy.DamageSEnemy (Damage);
 				Debug.Log ("We hit " + hit.collider.name + " and did " + Damage + " damage.");
 			} else if (enemy != null) {
-				enemy.DamageEnemy(Damage);
+				enemy.DamageEnemy (Damage);
 				Debug.Log ("We hit " + hit.collider.name + " and did " + Damage + " damage.");
 			}
 		}
+		if (Time.time >= timeToSpawnEffect) {
+			Vector3 hitPos;
+			Vector3 hitNormal;
+			if (hit.collider == null) {
+				hitPos = (mousePosition - firePointPosition) * 30;
+				hitNormal = new Vector3 (9999, 9999, 9999);
+			} else {
+				hitPos = hit.point;
+				hitNormal = hit.normal;
+			}
+			Effect (hitPos, hitNormal);
+			timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
+		}
+
 	}
 
-	void Effect () {
-		Instantiate (BulletTrailPrefab, firePoint.position, firePoint.rotation);
+	void Effect (Vector3 hitPos, Vector3 hitNormal) {
+		Transform trail = Instantiate (BulletTrailPrefab, firePoint.position, firePoint.rotation) as Transform;
+		LineRenderer lr = trail.GetComponent<LineRenderer> ();
+
+		if(lr != null){
+			lr.SetPosition (0, firePoint.position);
+			lr.SetPosition (1, hitPos);
+		}
+
+		Destroy (trail.gameObject, 0.04f);
+		if (hitNormal != new Vector3 (9999, 9999, 9999)) {
+			Transform hitParticle = Instantiate (HitPrefab, hitPos, Quaternion.FromToRotation (Vector3.right, hitNormal));
+			Destroy (hitParticle.gameObject, 1f);
+		}
 		Transform clone = Instantiate (MuzzleFlashPrefab, firePoint.position, firePoint.rotation) as Transform;
 		clone.parent = firePoint;
 		float size = Random.Range (0.6f, 0.9f);
